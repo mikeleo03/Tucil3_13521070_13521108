2b603b1f1841d0eafe1e1f5c9e914ba59e0b17e5
Michael Leon Putra Widhi <leonmichael463@gmail.com>
2023-04-06 10:03:12 +0700

    intantiate JSON payloads

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
-// Beberapa variabel global
-let adjMatrix = []
+// Impor fungsi dari file algorithm
+// import { AStar } from './algorithm.js';
+
+// Deklarasi variabel global
+let myMap;
+let adjMatrix = [];
+let weightGraph = [];
+let posList = [];
 
 // Berkaitan dengan setup awal web
 window.onload = function() {
@@ -9,4 +15,35 @@ window.onload = function() {
   L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
   }).addTo(myMap);
+}
+
+// Penerimaan dan pembaacan konfigurasi peta dari masukan
+function readFile () {
+  // Beberapa kasus ujung penerimaan file
+  let input = document.getElementById('inputFile');
+  if (!input.files[0]) {
+    alert("Please select a file!");
+  } else {
+    let file = input.files[0];
+    let reader = new FileReader();
+    reader.readAsText(file);
+    reader.onload = initiateSearch;
+  }
+
+  // Instansiasi onload dengan JSON
+  function initiateSearch (e) {
+    // Informasi dari masukan
+    infoParsed = JSON.parse(e.target.result.toString());
+    adjMatrix = infoParsed.adjMatrix;
+    weightGraph = infoParsed.weightGraph;
+    posList = infoParsed.posList;
+
+    // Inisiasi penandaan posisi korrdinat pada peta
+
+    // Melakukan penanganan terhadap pengisian simpul yang akan dijelajah
+
+    // Menangani tabel daftar simpul dan jaraknya
+
+    // Menggambar petak path
+  }
 }
