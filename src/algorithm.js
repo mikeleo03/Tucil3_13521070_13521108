e32ad4f65d5ded6e535db5362edb79a0c7f5bd2f
Michael Leon Putra Widhi <leonmichael463@gmail.com>
2023-04-06 09:17:27 +0700

    better algo setup
    better algo setup

--- src/algorithm.js
+++ src/algorithm.js
@@ -1,40 +1,69 @@
-/* File untuk melakukan pemrosesan terhadap node */
+/* File untuk melakukan pemrosesan terhadap simpul */
 /* Berbagai deklarasi data akan dijabarkan disini */
+
+// Inisiasi konstanta tak hingga
+// Fun-fact : jarak 2 titik terjauh di bumi 7590 km, jadi ga akan mungkin ada yang 10000
+const INF = 10000; // dalam km
 
 // Kelas Node, untuk inisiasi sebuah simpul yang akan dianalisis
 class Node {
     // 1. Konstruktor
-    constructor(nilai, priority) {
+    constructor (nilai, priority) {
         this.nilai = nilai
         this.priority = priority
     }
+
+    // 2. Getter nilai
+    getNilai () {
+        return this.nilai;
+    }
+
+    // 3. Getter priority
+    getPrio () {
+        return this.priority;
+    }
 }
 
-// Kelas PQ berupa priorityqueue, untuk melakukan pemilihan node dengan bobot terkecil
+// Kelas PQ berupa priorityqueue, untuk melakukan pemilihan simpul dengan bobot terkecil
 class PQ {
     // 1. Konstruktor
-    constructor() {
+    constructor () {
         this.queue = [];
     }
 
-    // 2. Swap, untuk menukar posisi
-    swap(pos1, pos2) {
+    // 2. Getter panjang queue
+    getLength () {
+        return this.queue.length;
+    }
+
+    // 3. Getter elemen queue
+    getElmt (idx) {
+        return this.queue[idx];
+    }
+
+    // 4. Setter elemen queue
+    setElmt (idx, val) {
+        this.queue[idx] = val;
+    }
+
+    // 5. Swap, untuk menukar posisi
+    swap (pos1, pos2) {
         let val = this.queue[pos1];
-        this.queue[pos1] = this.queue[pos2];
-        this.queue[pos2] = val;
+        this.setElmt(pos1, this.getElmt(pos2));
+        this.setElmt(pos2, val);
         // Mengembalikan isi queue setelah ditukar
         return this.queue;
     }
 
-    // 3. Enquque, prosedur untuk menambahkan antrian
+    // 6. Enquque, prosedur untuk menambahkan antrian
     // Ingat perlu juga untuk mempertimbangkan prioritas
-    enqueue(nilai) {
+    enqueue (nilai) {
         this.queue.push(nilai);
         // Melakukan pemrosesan pemasukan
-        let idx = this.queue.length - 1
-        while(idx > 0) {
+        let idx = this.getLength() - 1
+        while (idx > 0) {
             let predecessor = Math.floor((idx - 1)/2);
-            if (this.queue[predecessor].priority > this.queue[idx].priority) {
+            if (this.getElmt(predecessor).getPrio() > this.getElmt(idx).getPrio()) {
                 this.swap(idx, predecessor);
                 idx = predecessor;
             } else {
@@ -44,33 +73,32 @@ class PQ {
         return this.queue;
     }
 
-    // 4. Dequeue, prosedur untuk menghapus elemen dari antrian prioritas
+    // 7. Dequeue, prosedur untuk menghapus elemen dari antrian prioritas
     // Ingat perlu mempertimbangkan prioritas
-    dequeue() {
+    dequeue () {
         // Pop di js unik, jadi harus pindah ke depan
-        const length = this.queue.length;
-        const prio = this.queue[0].priority;
+        const prio = this.getElmt(0).getPrio();
         this.swap(length - 1, 0);
 
         let value = this.queue.pop();
         // Perhatikan solusi pop
-        if (this.queue.length > 1) {
+        if (this.getLength() > 1) {
             let predecessor = 0;
             while (idxswap !== null) {
                 let leftPart = 2 * predecessor;
                 let rightPart = 2 * predecessor;
                 idxswap = null;
                 // Penanganan untuk sisi partisi kiri
-                if (leftPart < length) {
-                    if (this.queue[leftPart].priority < prio) {
+                if (leftPart < this.getLength()) {
+                    if (this.getElmt(leftPart).getPrio() < prio) {
                         idxswap = leftPart;
                     }
                 }
 
                 // Penanganan untuk sisi partisi kanan
-                if (rightPart < length) {
-                    if ((this.queue[rightPart].priority < prio && idxswap === null) ||
-                    (this.queue[rightPart].priority < this.queue[leftPart].priority && idxswap !== null)) {
+                if (rightPart < this.getLength()) {
+                    if ((this.getElmt(rightPart).getPrio() < prio && idxswap === null) ||
+                    (this.getElmt(rightPart).getPrio() < this.getElmt(leftPart).getPrio() && idxswap !== null)) {
                         idxswap = rightPart
                     }
                 }
@@ -87,4 +115,96 @@ class PQ {
             return value;
         }
     }
+}
+
+// Fungsi heuristics, untuk melakukan kalkulasi nilai h(n)
+// Yaitu straight line distance dari simpul n ke finish
+// Inspired by https://www.geeksforgeeks.org/program-distance-two-points-earth/
+function heuristics (posList, final, initial) {
+    // Mengambil informasi dari masukan
+    // Posisi awal
+    let lintang1 = posList[initial].lintang * Math.PI / 180;
+    let bujur1 = posList[initial].bujur * Math.PI / 180;
+    // Posisi akhir
+    let lintang2 = posList[final].lintang * Math.PI / 180;
+    let bujur2 = posList[final].bujur * Math.PI / 180;
+
+    // Perhitungan
+    let r = 6371
+    let dLintang = lintang2 - lintang1
+    let dBujur = bujur2 - bujur1
+
+    // Haversine formula
+    let a = Math.pow(Math.sin(dLintang / 2), 2)
+            + Math.cos(lintang1) * Math.cos(lintang2)
+            * Math.pow(Math.sin(dBujur / 2),2);
+    let c = 2 * Math.asin(Math.sqrt(a));
+
+    return r * c;
+}
+
+// Fungsi AStar, menjalankan algoritma A*
+// prioritas berdasarkan f(n) = g(n) + h(n)
+// dengan g(n) adalah jarak dari start ke n
+// h(n) adalah straight line distance dari simpul n ke finish
+function AStar (start, finish, adjMatrix, weightGraph, posList) {
+    // Inisiasi
+    gVals = new Map();          // Nilai g(n) dari semua simpul yang hidup
+    pred = new Map();           // Simpul yang terhubung dengan n
+    fVals = new Map();          // Nilai f(n), yaitu g(n) + h(n)
+    listActiveNode = new PQ();  // Simpul aktif kemudian disimpan dalam PriorityQueue untuk diatur prioritasnya
+
+    // Karena proses dimulai dari awal, maka jarak semua simpul dari start takhingga
+    for (let i = 0; i < adjMatrix.length; i++) {
+        gVals.set((i + 1).toString(), INF);
+        fVals.set((i + 1).toString(), INF);
+    }
+    // Kecuali simpul awal
+    gVals.set(start, 0);
+    fVals.set(start, heuristics(posList, finish, start)); // fungsi heuristics blom dibuat
+
+    // Simpul awal akan dimasukkan dalam antrian prioritas fVals
+    listActiveNode.enqueue(new Node(start, heuristics(posList, finish, start)));
+
+    // Selama masih ada simpul aktif
+    while (listActiveNode.getLength() > 0) {
+        let current = listActiveNode.getElmt(0);
+
+        // Kalo path sedang ada di simpul hidup, buat grafnya
+        if (current.getNilai() == finish) {
+            let dist = gVals.get(current);
+            let finalPath = [current];
+
+            // Proses rekonstruksi graf hasil
+            while (pred.has(current)) {
+                current = pred.get(current);
+                finalPath.unshift(current); // unshift == insert first
+            }
+
+            // distance yang dikembalikan sudah pasti jarak total
+            return [finalPath, dist];
+        }
+
+        // Jika tidak, maka siap untuk pemrosesan simpul
+        // Pemrosesan dilakukan terhadap tetangganya
+        let neighbors = parseInt(current.getNilai()) - 1
+        for (let i = 0; i < neighbor.length; i++) {
+            // Kalo bertetangga
+            if (neighbors[i] == 1) {
+                let neighbor = (i + 1).toString();
+
+                // Kalo g(n) lebih besar dari cost ke tetangga, switch ke tetangga
+                let gValsNow = gVals.get(current.getNilai()) + weightGraph[neighbors][i];
+                if (gValsNow < gVals.get(neighbor)) {
+                    pred.set(neighbor, current.getNilai());
+                    gVals.set(neighbor, gValsNow);
+                    fVals.set(neighbor, gValsNow + heuristics(posList, finish, start));
+                    listActiveNode.enqueue(new Node(neighbor, gValsNow + heuristics(posList, finish, start)));
+                }
+            }
+        }
+    }
+
+    // Penanganan jika tidak ada jalur yang menghubungkan
+    return [];
 }
--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
+// Beberapa variabel global
+let adjMatrix = []
+
+// Berkaitan dengan setup awal web
 window.onload = function() {
   document.getElementsByClassName('map')[0].style.display = 'block';
-
   myMap = L.map('map').setView([0, 0], 1);
-  myMap.doubleClickZoom.disable();
 
   L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
