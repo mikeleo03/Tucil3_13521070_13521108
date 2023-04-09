4da90bec8ffccc0d753a1f5fce53e7a82354e837
Michael Leon Putra Widhi <leonmichael463@gmail.com>
2023-04-09 18:55:29 +0700

    clearer program

--- src/main.js
+++ src/main.js
@@ -1,6 +1,4 @@
 // Impor fungsi dari file algorithm
-// import { AStar } from './algorithm.js';
-// const AStar = require('./algorithm.js');
 
 // Deklarasi variabel global
 let maps;
@@ -66,8 +64,8 @@ window.onload = function() {
   })
 }
 
-// Penerimaan dan pembaacan konfigurasi peta bonus dari masukan
-function readFileBonus () {
+// Penerimaan dan pembaacan konfigurasi peta dari masukan
+function readFile () {
   // Beberapa kasus ujung penerimaan file
   realmap = true;
   let input = document.getElementById('inputFile');
@@ -90,32 +88,6 @@ function readFileBonus () {
 
     startMapSearch();   // Inisiasi penandaan posisi koordinat pada peta
     drawPathLine();     // Menggambar petak path
-    tablePathControl(); // Menangani tabel daftar simpul dan jaraknya
-    chooseNode();       // Melakukan penanganan terhadap pengisian simpul yang akan dijelajah
-  }
-}
-
-// Penerimaan dan pembaacan konfigurasi peta dari masukan
-function readFile () {
-  realmap = false;
-  // Beberapa kasus ujung penerimaan file
-  let input = document.getElementById('inputFile');
-  if (!input.files[0]) {
-    alert("Please select a file!");
-  } else {
-    let file = input.files[0];
-    let reader = new FileReader();
-    reader.onload = initiateSearch;
-    reader.readAsText(file);
-  }
-
-  // Instansiasi onload dengan JSON
-  function initiateSearch (e) {
-    // Informasi dari masukan
-    infoParsed = JSON.parse(e.target.result.toString());
-    posList = infoParsed.posList;
-    adjMatrix = infoParsed.adjMatrix;
-
     tablePathControl(); // Menangani tabel daftar simpul dan jaraknya
     chooseNode();       // Melakukan penanganan terhadap pengisian simpul yang akan dijelajah
   }
